38ccc0170af3d864a92b139e9237a1ef1bc810f6
Kaleb Williams <kalebklw@gmail.com>
2025-07-29 01:18:36 +0000

    Created the arrays for the faces and the suites of the card, as well as the randomization of the faces and suites upon clicking the button.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here 
+  function drawCard() {
+
+    const div = document.querySelector(".div");
+  
+    const arrayOfFaces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const arrayOfSuites = ["♦", "♥", "♠", "♣"];
+    
+  
+    function randomIndex (array) {
+      return Math.floor(Math.random() * array.length);
+    };
+    
+
+    div.innerHTML = arrayOfFaces[randomIndex(arrayOfFaces)] + arrayOfSuites[randomIndex(arrayOfSuites)]
+  };
+
+  const interactiveButton = document.querySelector(".interactive");
+    interactiveButton.addEventListener ("click",
+      drawCard
+  );
 };
+
+
+
