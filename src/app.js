0bc0326b9c0ebf34090981cec9e965829ec33ae6
Kaleb Williams <kalebklw@gmail.com>
2025-07-31 16:12:11 +0000

    At finish line, just need to double check code.

--- src/app.js
+++ src/app.js
@@ -6,29 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here 
+  
   function drawCard() {
-
-    const div = document.querySelector(".div");
-  
-    const arrayOfFaces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    const arrayOfSuites = ["♦", "♥", "♠", "♣"];
+    const faceOfCard = document.querySelector("#face");
+    const suitOfCard = document.querySelectorAll(".suit");
     
-  
     function randomIndex (array) {
       return Math.floor(Math.random() * array.length);
     };
     
-
-    div.innerHTML = arrayOfFaces[randomIndex(arrayOfFaces)] + arrayOfSuites[randomIndex(arrayOfSuites)]
+    const arrayOfFaces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const randomFaceIndex = randomIndex(arrayOfFaces)
+    const arrayOfSuites = ["♦", "♥", "♠", "♣"];
+    const randomSuitIndex = randomIndex(arrayOfSuites)
+      
+    faceOfCard.innerHTML = arrayOfFaces[randomFaceIndex];
+    suitOfCard.forEach((item) => {
+    const suit = arrayOfSuites[randomSuitIndex];
+    item.innerHTML = suit;
+    item.style.color = (suit === "♥" || suit === "♦") ? "red" : "black";
+    }
+  );
   };
+  drawCard();
+  // End of drawCard Function
+
+
 
   const interactiveButton = document.querySelector(".interactive");
+  interactiveButton.style.backgroundColor = "silver";
     interactiveButton.addEventListener ("click",
       drawCard
   );
-  drawCard();
+
+
+
 };
-
-
-
